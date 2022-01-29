c132633b439935ee45964451239883d11480015b
Siavash Shojaee <48875988+SiavashSkynet@users.noreply.github.com>
2022-01-29 21:17:29 +0330

    Create db.cpp

---
+++ db.cpp
@@ -0,0 +1,17 @@
+#include "db.h"
+
+int db_handler()
+{
+	sqlite3* DB;
+	int exit = 0;
+	exit = sqlite3_open("example.db", &DB);
+
+	if (exit) {
+		std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+		return (-1);
+	}
+	else
+		std::cout << "Opened Database Successfully!" << std::endl;
+	sqlite3_close(DB);
+	return (0);
+}
