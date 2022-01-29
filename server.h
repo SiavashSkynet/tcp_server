689441b3390e30baafbae6054c7a01208feb4fe9
Siavash Shojaee <48875988+SiavashSkynet@users.noreply.github.com>
2022-01-29 21:08:36 +0330

    Update server.h

--- server.h
+++ server.h
@@ -1,1 +1,33 @@
 
+
+
+#include <stdio.h>
+#include <string.h> 
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h> 
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h> 
+
+#define TRUE 1
+#define FALSE 0
+#define PORT 9000
+
+
+    int opt = TRUE;
+    int master_socket , addrlen , new_socket , client_socket[30] ,
+        max_clients = 30 , activity, i , valread , sd;
+    int max_sd;
+    struct sockaddr_in address;
+
+    char buffer[1025]; //data buffer of 1K
+
+    //set of socket descriptors
+    fd_set readfds;
+
+    //a message
+    char *message = "ECHO Daemon v1.0 \r\n";
+
