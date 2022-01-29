e1c355e2f6ce48d552097a9ff5787db83871d493
Siavash Shojaee <48875988+SiavashSkynet@users.noreply.github.com>
2022-01-29 21:12:39 +0330

    Create read_config.cpp

---
+++ read_config.cpp
@@ -0,0 +1,39 @@
+#include "read_config.h"
+
+static int handler(void* user, const char* section, const char* name,
+    const char* value)
+{
+    configuration* pconfig = (configuration*)user;
+
+#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+    if (MATCH("address", "port"))
+    {
+        pconfig->port = atoi(value);
+    }
+    else if (MATCH("prefix", "p1"))
+    {
+        pconfig->p1 = atoi(value);
+    }
+    else if (MATCH("prefix", "p2"))
+    {
+        pconfig->p2 = _strdup(value);
+    }
+    else
+    {
+        return 0;
+    }
+    return 1;
+}
+ int read_ini()
+ {
+   configuration config;
+
+      if (ini_parse("fconfig.ini", handler, &config) < 0)
+      {
+          printf("Can't load 'fconfig.ini'\n");
+          return 1;
+      }
+
+      printf("Config loaded from 'fconfig.ini': port=%d, name=%d, email=%s\n", config.port, config.p1, config.p2);
+      return 0;
+ }
