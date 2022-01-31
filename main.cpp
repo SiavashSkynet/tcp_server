6f142610d1a242f78f95ac164df896e982e1783c
Siavash Shojaee <48875988+SiavashSkynet@users.noreply.github.com>
2022-01-31 20:24:01 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
-
+#include <iostream>
 #include <stdio.h>
+
 #include "server.h"
 #include "read_config.h"
 #include "db.h"
-#include <iostream>
 #include "time.h"
 
-using namespace std;
 
+using namespace std;
+int port ;
 int main()
 {
+    // read config
+    port = read_config::read_ini();
+
+    // add time interval
+    // create a soccket
+    db::db_handler();
+    server::handle_server(port);
+    
+    
+    // connect to db
+    
+    // decode the message from json to a string and int
+    // create a stack of tcp_client t
 }
