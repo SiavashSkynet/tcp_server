a6ca94bae3230f158096d186484816f3a3573c4d
Siavash Shojaee <48875988+SiavashSkynet@users.noreply.github.com>
2022-01-31 21:10:13 +0330

    add table create functions

--- db.h
+++ db.h
@@ -7,4 +7,6 @@ namespace db
   void insert_data_table_p1(int p1 , String time);
   void insert_data_table_p2(String p2 , String time);
   string sort_data();
+  void create_table_p1();
+  void create_table_p2();
 }
