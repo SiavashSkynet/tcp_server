c36956a7c997698953260331451c87e5d56f02c2
Siavash Shojaee <48875988+SiavashSkynet@users.noreply.github.com>
2022-01-31 21:28:33 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,22 +6,18 @@
 #include "db.h"
 #include "time.h"
 
-
 using namespace std;
+
 int port ;
+
 int main()
 {
-    // read config
+   
     port = read_config::read_ini();
-
-    // add time interval
-    // create a soccket
-    db::db_handler();
     server::handle_server(port);
+    db::db_handler();
     
-    
-    // connect to db
-    
-    // decode the message from json to a string and int
-    // create a stack of tcp_client t
+    /*
+    after interval = 2mins sort db here
+    */
 }
