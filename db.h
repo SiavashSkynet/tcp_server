17de80f28d79ee100a7e79701ebf526cad9e1f4f
Siavash Shojaee <48875988+SiavashSkynet@users.noreply.github.com>
2022-02-01 21:02:55 +0330

    Update db.h

--- db.h
+++ db.h
@@ -4,6 +4,7 @@
 namespace db
 {
   bool db_connected = false;
+  bool db_connected_ptr = &db_connected;
   int db_handler();
   void insert_data_table_POS_MESSAGE(int p1 , String p2, String current_time);
   string sort_data();
